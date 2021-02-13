86d4c62df74201dc5fadf120b7c8814aacaed7e0
Khushboo Shah <shahkhushboo0@gmail.com>
2021-02-12 20:52:04 -0500

    Updated in pom.xml & removing unused imports

--- portal/src/main/java/com/application/portal/repository/UserRepository.java
+++ portal/src/main/java/com/application/portal/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.application.portal.repository;
 import com.application.portal.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface UserRepository extends MongoRepository<User,String>,UserRepositoryCustom {
 
      User findByFirstName(String firstName);
